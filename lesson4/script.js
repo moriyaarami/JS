f919094cec1f6dbb5b596c3bd9048ee03ae0ba42
moriya <moriyastudent@gmail.com>
2024-02-06 00:16:52 +0200

    added

--- lesson4/script.js
+++ lesson4/script.js
@@ -34,10 +34,52 @@ function enter() {
 
     const un = document.getElementById("userName").value;
     const pw = document.getElementById("password").value;
+    const output2 = document.getElementById("output2");
     if (un == "banana" && pw == "a123") {
-        document.getElementById("output2").innerHTML = "ברוך הבא";
+        output2.innerHTML = "ברוך הבא";
+        output2.style.color = "green";
     }
     else {
-        document.getElementById("output2").innerHTML = "יש טעות באחד הפרטים";
+        output2.innerHTML = "יש טעות באחד הפרטים";
+        output2.style.color = "red";
     }
 }
+function tast1() {
+    const num = document.getElementById("number").value;
+    const number = document.getElementById("number");
+    /*  const number = +num.value; */
+
+    if (num <= 40) {
+        number.style.backgroundColor = "red";
+    } else if (num <= 60) {
+        number.style.backgroundColor = "orange";
+    } else if (num <= 80) {
+        number.style.backgroundColor = "blue";
+    } else if (num <= 100) {
+        number.style.backgroundColor = "green";
+    }
+}
+/* function replace() {
+    let num1 = document.getElementById("str1").value;
+    let num2 = document.getElementById("str2").value;
+
+    str1 = str2;
+    str2 = num1;
+} */
+
+function salerycalcultor() {
+    let name = document.getElementById("user").value;
+    let num = document.getElementById("salery").value;
+    let total10 = +num + +num / 10;
+    let total5 = +num + (+num / 10) / 2;
+
+    /*  let output3 = document.getElementById("output3"); */
+
+    if (total10 <= 6000) {
+        document.getElementById("output3").innerHTML = total10;
+    } else if (total10 > 6000) {
+        document.getElementById("output3").innerHTML = total5;
+    }
+
+}
+
