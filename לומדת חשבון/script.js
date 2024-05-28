8cbb3432e85836e6d7582042c38cf97ebdce4658
moriya <moriyastudent@gmail.com>
2024-05-28 11:20:47 +0300

    operator

--- לומדת חשבון/script.js
+++ לומדת חשבון/script.js
@@ -1,21 +1,61 @@
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-
-const container = document.querySelector('#container');
+const div = document.createElement('div');
+const n1 = document.createElement('span');
+const operator = document.createElement('span');
+operator.className = 'operator';
+const n2 = document.createElement('span');
+const task = document.querySelector('.task');
+task.appendChild(div);
+div.appendChild(n1);
+div.appendChild(operator);
+div.appendChild(n2);
 let result;
 let score = 0;
+let thisOptionNumber = 0;
+
+const optionsOperator = ['+', '-', '*', ':'];
+
+function Operators() {
+    const selectElem = document.getElementById('operator');
+    selectElem.addEventListener('change', () => {
+        const index = selectElem.selectedIndex;
+
+        if (index == 0 || index == 1) {
+
+            text(thisOptionNumber, optionsOperator[0]);
+        } else if (index == 2) {
+
+            text(thisOptionNumber, optionsOperator[1]);
+        } else if (index == 3) {
+
+            text(thisOptionNumber, optionsOperator[2]);
+        } else if (index == 4) {
+
+            text(thisOptionNumber, optionsOperator[3]);
+        }
+    });
+
+}
+
+Operators();
 
 /* פונקציה שיוצרת תרגיל חשבון */
-function text(n) {
-    num1.innerHTML = Math.floor(Math.random() * n) + 1;
-    num2.innerHTML = Math.floor(Math.random() * n) + 1;
-    input.value = '';
+function text(n, o) {
+    operator.innerHTML = o;
+    n1.innerHTML = Math.floor(Math.random() * n) + 1;
+    n2.innerHTML = Math.floor(Math.random() * n) + 1;
 
+    input.value = '';
 }
 
 
 function check() {
-    result = +num1.innerHTML + +num2.innerHTML;
+
+    result = +n1.innerHTML + + n2.innerHTML;
+
+    /*   result = `${+n1.innerHTML} ${op} ${+n2.innerHTML}`; */
+    console.log(result);
+
+
     const input = document.querySelector('#input').value;
 
     if (input == result) {
@@ -28,7 +68,6 @@ function check() {
         Score(false);
     }
 
-
 }
 
 function Result(answer) {
@@ -38,53 +77,64 @@ function Result(answer) {
     if (answer) {
         p.innerHTML = 'תשובה נכונה';
         setTimeout(() => {
-            p.innerHTML = ''
-            options();
+            p.innerHTML = '';
+            text(thisOptionNumber);
         }, 1000);
-
 
     } else {
         p.innerHTML = 'תשובה לא נכונה';
         setTimeout(() => {
-            p.innerHTML = ''
-            text();
+            p.innerHTML = '';
+            text(thisOptionNumber)
+
         }, 1000);
 
     }
 
     container.appendChild(p);
     Score();
-
 }
 
-function options() {
+function optionsNumbers() {
 
     const selectElem = document.getElementById('numbers');
     selectElem.addEventListener('change', () => {
         const index = selectElem.selectedIndex;
-        const value = selectElem[index].value;
-        console.log(index, value);
-        if (index === 1) {
-            text(10);
-            check();
+        if (index == 0) {
+            thisOptionNumber = -1;
+            text(thisOptionNumber);
+        } else if (index === 1) {
+            thisOptionNumber = 10;
+            text(thisOptionNumber);
+
         } else if (index === 2) {
-            text(100);
+            thisOptionNumber = 100;
+            text(thisOptionNumber);
+
         } else if (index === 3) {
-            text(500);
+            thisOptionNumber = 500;
+            text(thisOptionNumber);
+
         } else if (index === 4) {
-            text(1000);
+            thisOptionNumber = 1000;
+            text(thisOptionNumber);
+
         }
 
-    })
+    });
+
+
 }
+
 
 
 /* יצירת טבלה לתשובות שהמשתמש ענה  */
 function table() {
+
     const tbody = document.querySelector('tbody');
     const tr = document.createElement('tr');
     tr.innerHTML = `
-    <td>${num2.innerHTML}+${num1.innerHTML}</td>
+    <td>${n2.innerHTML}+${n1.innerHTML}</td>
     <td>${result}</td>
     <td>${input.value}</td>
     <td>${input.value == result ? '5' : '0'}</td>
@@ -103,5 +153,11 @@ function Score(res) {
     }
     showScore = document.querySelector('.score');
     showScore.innerHTML = score;
+}
+
+function reset() {
+    score = 0;
+    showScore.innerHTML = score;
+    text(0);
 }
 
